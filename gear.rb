cc10fa3cc15094fdac5ae50b1d420871bd06951e
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2024-12-08 18:17:10 +0000

    ADD: create a wheel class and SRP is maintained

--- gear.rb
+++ gear.rb
@@ -1,10 +1,10 @@
 class Gear
-	attr_reader :chainring, :cog, :rim, :tire
+	attr_reader :chainring, :cog, :wheel
   
-	def initialize(cog, chainring, rim, tire)
+	def initialize(cog, chainring,wheel=nil)
 		@cog = cog
 		@chainring = chainring
-		@wheel=Wheel.new(rim, tire)
+		@wheel=wheel
   end
 	
 	def ratio
@@ -15,20 +15,34 @@ class Gear
 		ratio*wheel.diameter
 	end
 
-	Wheel = Struct.new(:rim, :tire) do
-		def diameter
-			rim + (tire*2)
-		end
-	end
+
 end
 
-#Gear.new(52,11,26,1.5).gear_inches
-#=> 6.134
+class Wheel
+	attr_reader :rim, :tire
+
+	def initialize(rim, tire)
+		@rim = rim
+		@tire = tire
+	end
+
+	def diameter
+		rim + (tire*2)
+	end
+
+	def circumference
+		diameter * Math::PI
+	end
+end
+#@wheel =  Wheel.new(26,15)
+# puts @wheel.circumference
+#puts Gear.new(52,11,@wheel).gear_inches
+
 
 #Gear.new(52,11).ratio
 #=>  wrong number of arguments (given 2, expected 4)
 
-# Notes +> altering the number of arguments that a menthod reqires breaks all th existing callers of the method
+# Notes => altering the number of arguments that a menthod reqires breaks all th existing callers of the method
 
 # An example of a disaster
 class Gear
