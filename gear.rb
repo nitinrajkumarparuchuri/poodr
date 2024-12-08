6f7d6018319bd1bb32fd3c8f8a89df6b4cdf72ec
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2024-12-08 18:09:13 +0000

    ADD: extract methods to have srp

--- gear.rb
+++ gear.rb
@@ -4,8 +4,7 @@ class Gear
 	def initialize(cog, chainring, rim, tire)
 		@cog = cog
 		@chainring = chainring
-		@rim = rim
-		@tire = tire
+		@wheel=Wheel.new(rim, tire)
   end
 	
 	def ratio
@@ -13,9 +12,14 @@ class Gear
 	end
 
 	def gear_inches
-		ratio*(rim+(tire*2))
+		ratio*wheel.diameter
 	end
 
+	Wheel = Struct.new(:rim, :tire) do
+		def diameter
+			rim + (tire*2)
+		end
+	end
 end
 
 #Gear.new(52,11,26,1.5).gear_inches
@@ -25,3 +29,22 @@ end
 #=>  wrong number of arguments (given 2, expected 4)
 
 # Notes +> altering the number of arguments that a menthod reqires breaks all th existing callers of the method
+
+# An example of a disaster
+class Gear
+  
+	def initialize(cog, chainring)
+		@cog = cog
+		@chainring = chainring
+	end
+	def ratio
+		@chainring/@cog.to_f  # <--Road to ruin
+	end
+end
+
+# Changing the values of instance variables like validating can become difficult
+
+def cog
+	@cog
+end
+#implementing the method changed the cog from data to a behavior
